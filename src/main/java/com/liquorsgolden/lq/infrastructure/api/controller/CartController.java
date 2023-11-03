20cbff8a002f983418b38e88d078c54ddce4d08b
Ismael Trocha <ismael.trocha@hotmail.com>
2023-11-03 00:04:08 -0500

    Reformat code

--- src/main/java/com/liquorsgolden/lq/infrastructure/api/controller/CartController.java
+++ src/main/java/com/liquorsgolden/lq/infrastructure/api/controller/CartController.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/api/cart/")
 @AllArgsConstructor
+@CrossOrigin(allowCredentials = "*")
 public class CartController {
 
   private final AddShoppingCartApplication addShoppingCartApplication;
